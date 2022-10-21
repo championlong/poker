d521ff7afc834095d058d71c291effc0019a49e6
chenglong <chenglong@gitlab.com>
2022-10-21 15:47:31 +0800

    feat:更新代码结构

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"net/http"
 	"poker/request"
 )
 
 const (
 	team = "艾斯奥特曼"
 )
-
-var client = &http.Client{}
-
 
 func main() {
 	a,err := request.CardInfo(context.Background())
